b4a608211c1c900f5ee4cc38a2f6eb7792c1cb92
zachooz <hoffking@gmail.com>
2018-10-08 23:03:24 -0400

    init

--- bin/activate.fish
+++ bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/zacharyhoffman/Desktop/Codeing/school/AI/Tron/Tron"
+set -gx VIRTUAL_ENV "/Users/zacharyhoffman/Desktop/Codeing/school/AI/Tron"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
