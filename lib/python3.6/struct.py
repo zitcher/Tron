4e1b654c3b549ed394fe810ae8f472a6cf83e958
zachooz <hoffking@gmail.com>
2018-10-08 23:13:53 -0400

    env

--- lib/python3.6/struct.py
+++ lib/python3.6/struct.py
@@ -1,1 +1,15 @@
-/usr/local/Cellar/python/3.6.5_1/Frameworks/Python.framework/Versions/3.6/lib/python3.6/struct.py
+__all__ = [
+    # Functions
+    'calcsize', 'pack', 'pack_into', 'unpack', 'unpack_from',
+    'iter_unpack',
+
+    # Classes
+    'Struct',
+
+    # Exceptions
+    'error'
+    ]
+
+from _struct import *
+from _struct import _clearcache
+from _struct import __doc__
